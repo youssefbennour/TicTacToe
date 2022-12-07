e81055936db0da72706e8e5aba0b95e48d35c062
youssefbennour <youssefbennour15@gmail.com>
2022-12-07 14:33:46 +0100

    first commit

---
+++ TicTacToe/MainGame.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TicTacToe
+{
+    internal class MainGame
+    {
+        public bool gameActive { get; set; }
+        private int activePlayer;
+        public string[] playerName = new string[2];
+        private char[] playersTags;
+        private char[] tags;
+        private char[,] tictac;
+
+        public MainGame(){
+            this.gameActive = true;
+            this.playerName[0] = this.playerName[1] = String.Empty;
+            this.playersTags = new char[2];
+            this.tags = new char[] { '1', '2', '3', 
+                                    '4', '5', '6', 
+                                    '7', '8', '9' };
+
+            this.tictac = new char[13, 15]{
+                {'-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'},
+                {'|',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|'},
+                {'|',' ',tags[0],' ','|',' ',' ',tags[1],' ','|',' ',' ',tags[2],' ','|'},
+                {'|',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|'},
+                {'-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'},
+                {'|',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|'},
+                {'|',' ',tags[3],' ','|',' ',' ',tags[4],' ','|',' ',' ',tags[5],' ','|'},
+                {'|',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|'},
+                {'-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'},
+                {'|',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|'},
+                {'|',' ',tags[6],' ','|',' ',' ',tags[7],' ','|',' ',' ',tags[8],' ','|'},
+                {'|',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|'},
+                {'-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'}
+            }; ;
+        }
+
+        public void displayMainMenu(){
+
+            try {
+                animateText("Enter player 1's name:",false);
+                this.playerName[0] = Console.ReadLine();
+            }catch(Exception InvalidChoiceException) {
+                this.playerName[0] = "Player1";
+            }
+
+            try {
+                animateText("Enter player 2's name:",false);
+                this.playerName[1] = Console.ReadLine();
+            } catch (Exception InvalidChoiceException) {
+                this.playerName[1] = "Player2";
+            }
+
+            while (this.playerName[0] == this.playerName[1]){
+                animateText("Enter a distinct name for player 2:", false,10);
+                this.playerName[1] = Console.ReadLine();
+            }
+            animateText("choosing randomly the first player to begin...",true);
+            Thread.Sleep(2000);
+
+            int firstPlayerIndex = new Random().Next(1, 100);
+            
+
+            if(firstPlayerIndex %2 == 1){
+                this.activePlayer = 1;
+            }else{
+                this.activePlayer = 0;
+            }
+
+            Console.WriteLine("The first player to start is: " + ((this.activePlayer == 1) ? this.playerName[1] : this.playerName[0]) + "\n\n");
+            Console.Write(playerName[activePlayer] + " choose your hero (X/O) : ");
+            char choice;
+            try {
+                choice = char.Parse(Console.ReadLine());
+            }catch (Exception InvalidChoiceException){
+                choice = 'X';
+            }
+
+            this.playersTags[activePlayer] = char.ToUpper(choice);
+
+            if (playersTags[activePlayer] == 'X'){
+                playersTags[(activePlayer + 1)% 2] = 'O';
+
+            }else{
+                playersTags[(activePlayer+ 1) % 2] = 'X';
+            }
+            
+            
+            
+        }
+
+        public void playerMove() {
+
+            displayGrid();
+            int cell = 0;
+            do {
+                Console.WriteLine("it's " + ((this.activePlayer == 1) ? this.playerName[1] : this.playerName[0]) + "'s turn choose an empty cell: ");
+                cell = int.Parse(Console.ReadLine());
+
+            } while (this.tags[cell - 1] == 'X' && this.tags[cell - 1] == 'O');
+
+            this.tags[cell - 1] = this.playersTags[this.activePlayer];
+            this.activePlayer = (this.activePlayer + 1) % 2;
+
+            this.tictac = new char[13, 15]{
+                {'-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'},
+                {'|',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|'},
+                {'|',' ',tags[0],' ','|',' ',' ',tags[1],' ','|',' ',' ',tags[2],' ','|'},
+                {'|',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|'},
+                {'-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'},
+                {'|',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|'},
+                {'|',' ',tags[3],' ','|',' ',' ',tags[4],' ','|',' ',' ',tags[5],' ','|'},
+                {'|',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|'},
+                {'-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'},
+                {'|',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|'},
+                {'|',' ',tags[6],' ','|',' ',' ',tags[7],' ','|',' ',' ',tags[8],' ','|'},
+                {'|',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|'},
+                {'-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'}
+            };
+            checkWinner();
+        }
+        public void displayGrid(){
+            for (int i = 0; i < 13; i++){
+                for (int j = 0; j < 15; j++){
+                    Console.Write(this.tictac[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void checkWinner()
+        {
+            //Check Horizontal cells
+            for (int i = 0; i < 9; i += 3){
+                if (this.tags[i] == this.tags[i + 1] && this.tags[i + 1] == this.tags[i + 2]){
+                    this.gameActive = false;
+                }
+            }
+
+            //Check vertical cells
+            for (int i = 2; i >= 0; i--){
+                if (this.tags[5-i] == this.tags[2-i] && this.tags[2- i] == this.tags[8 - i]){
+                    this.gameActive = false;
+                }
+            }
+
+            //Check Diaognal cells
+            if ((tags[0] == tags[4] && tags[4] == tags[8]) || tags[2] == tags[4] && tags[4] == tags[6]){
+                this.gameActive = false;
+            }
+
+            if(!this.gameActive){
+                winnerMessage();
+                Thread.Sleep(3000);
+            }
+        }
+
+        public void winnerMessage(){
+            Console.Clear();
+            displayGrid();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            animateText(((this.activePlayer == 1) ? this.playerName[1] : this.playerName[0]) + " is The winner, Congrats!\n",true, 50);
+            Console.ResetColor();
+        }
+
+        
+
+        private void animateText(string text, bool nl, int speed = 40) {
+            foreach (char c in text) {
+                Console.Write(c);
+                Thread.Sleep(speed);
+            }
+
+            if (nl) {
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Diagnostics;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void coloredMessage(ConsoleColor color, string text_message) {
+            Console.ForegroundColor = color;
+            Console.Write(text_message);
+            Console.ResetColor();
+        }
+        static void Main(string[] args)
+        {
+            MainGame tic = new MainGame();
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + ("Welcome to TicTacToe".Length / 2)) + "}", "Welcome to TicTacToe"));
+                Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + ("Press 'S' to start a new game".Length / 2)+5) + "}", "Press 'S' to start a new game"));
+                Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + ("Press 'Q' to quit the game".Length / 2)+3) + "}", "Press 'Q' to quit the game"));
+                Console.Write(String.Format("{0," + ((Console.WindowWidth / 2) + ("------Your choice: ".Length / 2)) + "}", "------Your choice: "));
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                char c;
+                try {
+                    c = char.Parse(Console.ReadLine());
+                }catch(Exception InvalidChoiceException) {
+                    c = '\0';
+                }
+                
+                Console.ResetColor();
+                while (char.ToUpper(c) != 'Q' && char.ToUpper(c) != 'S')
+                {
+
+                    coloredMessage(ConsoleColor.Red, "Please enter a valid choice : ");
+                    try {
+                        c = char.Parse(Console.ReadLine());
+                    } catch (Exception InvalidChoiceException) {
+                        c = '\0';
+                    }
+                }
+                if (c == 'q' || c == 'Q') {
+                    Environment.Exit(1);
+                } else {
+                    tic.displayMainMenu();
+                    while (tic.gameActive == true) {
+                        tic.playerMove();
+                        Console.Clear();
+                    }
+                }
+            }
+            
+        }
+    }
+}
